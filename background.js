4393295fe1ba64b593386bd253770589cdf520a1
paytondev <81662879+paytontech@users.noreply.github.com>
2022-03-15 09:05:45 -0400

    get url

---
+++ background.js
@@ -0,0 +1,7 @@
+function getURL() {
+    async function getCurrentTab() {
+        let queryOptions = { active: true, currentWindow: true };
+        let [tab] = await chrome.tabs.query(queryOptions);
+        return tab.url;
+      }    
+}
